95a9ff7a8038a198406d75e4d86c76a5725c661b
droid-sentry <prod.droidsentry@gmail.com>
2025-10-27 03:29:49 +0900

    fix(app): システムオーナーIDを更新

--- apps/web/app.config.ts
+++ apps/web/app.config.ts
@@ -2,5 +2,5 @@ export const AppConfig = {
   title: "Deal Property",
   description: "Deal Propertyは、物件管理システムです。",
   copyright: "Tailor Base 2025.09",
-  systemOwnerIds: ["gp6DeoF2a6", "e8RZznFCAs"],
+  systemOwnerIds: ["gp6DeoF2a6", "KkmGMKCPkH"],
 };
