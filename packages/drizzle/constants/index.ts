804e1971d3b6e6d29bf4243a8c7095959fc6b313
droid-sentry <prod.droidsentry@gmail.com>
2025-11-08 12:05:06 +0900

    refactor(constants): remove deprecated constants file and update import paths for improved clarity and maintainability

--- packages/drizzle/constants/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "../../utils/src/constants/contract-type";
-export * from "../../utils/src/constants/company-b";
-export * from "../../utils/src/constants/broker-company";
-export * from "../../utils/src/constants/document-status";
-export * from "../../utils/src/constants/organization";
-export * from "../../utils/src/constants/account-company";
-export * from "../../utils/src/constants/bank-account";
